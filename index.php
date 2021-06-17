8ad19e41313c9add5dce694b5a4eb73dd5e2b811
arfanmaulanaw <83159326+arfanmaulanaw@users.noreply.github.com>
2021-06-17 10:08:01 +0700

    REST-API
    rest api

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         $respon[] = array(
             'status' => 'OK',
             'kode' => '999',
-            'data' => $datanya
+            'data' => $data
         ); 
 
         header("Context-type: application/json");
